eb09356195f0980c00c719bae584b2159902e042
Eric Chu <eric@ericchu.net>
2017-10-13 21:22:31 -0400

    Add list func

--- todo.py
+++ todo.py
@@ -11,17 +11,31 @@ def add_task(task):
         print(task, file=f)
 
 
-def add(args):
+def add_func(args):
     task = ' '.join(args.task)
     add_task(task)
+
+
+def list_tasks():
+    with open(TODOTXT, mode='r', encoding='utf-8') as f:
+        for i, task in enumerate(f):
+            print(i, task.strip())
+
+
+def list_func(args):
+    list_tasks()
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
+
     parser_add = subparsers.add_parser('add', aliases=['a'])
     parser_add.add_argument('task', nargs=argparse.REMAINDER)
-    parser_add.set_defaults(func=add)
+    parser_add.set_defaults(func=add_func)
+
+    parser_list = subparsers.add_parser('list', aliases=['l'])
+    parser_list.set_defaults(func=list_func)
     return parser.parse_args()
 
 
